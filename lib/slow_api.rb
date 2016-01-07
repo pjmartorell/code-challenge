a15a3a39d6870c1fbbb62f5d9b2a1200579ab37d
Pere Joan Martorell <perejoan@itnig.net>
2016-01-07 19:35:05 +0100

    Speed up slowapi test using a thread pool to maximize throughput

--- lib/slow_api.rb
+++ lib/slow_api.rb
@@ -1,14 +1,29 @@
 require "open-uri"
 require "socket"
+require "thread"
 
 module SlowApi
   URL = "http://www.slowapi.com/delay/0.3"
+  MAX_THREADS = 10
 
   def self.retrieve(requests, io=$stdout)
-    begin
-      requests.times { io.puts open(URL).read }
-    rescue SocketError => e
-      puts "Couldn't retrieve data from \"#{ URL }\""
-    end
+    requests_queue, buffer = Queue.new, Queue.new
+    requests.times.to_a.each{|r| requests_queue.push r }
+
+    MAX_THREADS.times.map do
+      Thread.new do
+        begin
+          while requests_queue.pop(true)
+            buffer << open(URL).read
+          end
+        rescue ThreadError => e
+          raise unless e.message =~ /queue empty/
+        end
+      end
+    end.each(&:join)
+    io.puts buffer.size.times.map { buffer.pop }
+
+  rescue SocketError => e
+    puts "Couldn't retrieve data from \"#{ URL }\""
   end
 end
