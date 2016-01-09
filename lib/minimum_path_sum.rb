5d7fb34e5c5b3c08eb6a000bb2ce65dd1afd645a
Pere Joan Martorell <perejoan@itnig.net>
2016-01-09 14:07:56 +0100

    Refactor minimum path sum test and its tests

--- lib/minimum_path_sum.rb
+++ lib/minimum_path_sum.rb
@@ -1,12 +1,23 @@
+require 'prime'
+
 module MinimumPathSum
-  def self.calculate(io=$stdin)
-    triangle = io.each_line.map { |line| line.split.map(&:to_i) }
-    height = triangle.length - 2
-    height.downto(0) do |y|
+  def self.calculate(io=$stdin, twisted=false)
+    @triangle = io.each_line.map { |line| line.split.map(&:to_i) }
+    previous_primes_indexes = get_primes_indexes(-1)
+
+    (@triangle.length - 2).downto(0) do |y|
+      current_primes_indexes = get_primes_indexes(y) if twisted
       y.downto(0) do |x|
-        triangle[y][x] += [triangle[y+1][x], triangle[y+1][x+1]].min
+        primes_indexes = previous_primes_indexes if twisted && @triangle[y][x] == 13
+        @triangle[y][x] += @triangle[y+1].values_at(x, x+1, *primes_indexes).min
       end
+      previous_primes_indexes = current_primes_indexes
     end
-    triangle[0][0]
+    @triangle[0][0]
+  end
+
+  private
+  def self.get_primes_indexes(row)
+    @triangle[row].each_with_index.select{|e,i| Prime.prime?(e)}.map(&:last)
   end
 end
--- spec/minimum_path_sum_spec.rb
+++ spec/minimum_path_sum_spec.rb
@@ -9,9 +9,52 @@ describe MinimumPathSum do
     2 4 6
     8 5 2 9
     eos
+
+    @triangle1 = <<-eos
+    3
+    14 13
+    13 23 22
+    9 5 3 2
+    eos
+
+    @triangle2 = <<-eos
+    3
+    13 14
+    22 21 11
+    1 1 1 1
+    eos
+
+    @triangle3 = <<-eos
+    13
+    20 13
+    13 20 20
+    20 20 20 13
+    13 20 20 20 20
+    eos
+
+    @triangle4 = <<-eos
+    13
+    3 11
+    eos
   end
 
-  it "finds the minimum path sum" do
-    expect(subject.calculate(@triangle)).to eq(13)
+  context 'when in normal mode' do
+    it "finds the minimum path sum" do
+      expect(subject.calculate(@triangle)).to eq(13)
+      expect(subject.calculate(@triangle1)).to eq(35)
+      expect(subject.calculate(@triangle2)).to eq(29)
+      expect(subject.calculate(@triangle3)).to eq(79)
+      expect(subject.calculate(@triangle4)).to eq(16)
+    end
+  end
+
+  context 'when in twisted mode' do
+    it "finds the minimum path sum" do
+      expect(subject.calculate(@triangle, true)).to eq(13)
+      expect(subject.calculate(@triangle1, true)).to eq(31)
+      expect(subject.calculate(@triangle2, true)).to eq(28)
+      expect(subject.calculate(@triangle3, true)).to eq(13*5)
+      expect(subject.calculate(@triangle4, true)).to eq(16)
+    end
   end
 end
